0700b2501979c0ca02cde480c8789e59affe247f
Shakil <shakil.hossain450s@gmail.com>
2022-10-25 10:39:11 +0600

    footer added

--- src/layout/MainLayout.js
+++ src/layout/MainLayout.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import Footer from "../Pages/Shared/Footer/Footer";
 import Header from "../Pages/Shared/Header/Header";
 
 const MainLayout = () => {
@@ -7,6 +8,7 @@ const MainLayout = () => {
     <div>
       <Header></Header>
       <Outlet></Outlet>
+      <Footer></Footer>
     </div>
   );
 };
