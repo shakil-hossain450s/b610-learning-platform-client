45eab20ed28b5f9ff7f62079f0456548f3233e34
Shakil <shakil.hossain450s@gmail.com>
2022-10-27 18:31:23 +0600

    enviroment variable added

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBL4QafVTEeOgF7nTQ1zJ-6gkNHAjh6HJg",
-  authDomain: "learning-website-client.firebaseapp.com",
-  projectId: "learning-website-client",
-  storageBucket: "learning-website-client.appspot.com",
-  messagingSenderId: "262244978979",
-  appId: "1:262244978979:web:64372fc8d4396928273c96",
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
